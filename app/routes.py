ec9334b3908264ee9b652da23d07be3005530812
Angel Gallegos <angel.gallegosx@gmail.com>
2016-03-19 14:40:06 +0200

    Receiving Data

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,31 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 @app.route('/')
 def index():
     user = {'nickname': 'stranger'}
     return render_template('index.html', title='Antti-Social Network', user=user)
 
-@app.route('/welcome')
+@app.route('/Statistics')
 def welcome():
     return render_template('welcome.html')
 
 @app.route('/login', methods=["GET", "POST"])
-def login(user):
+def login():
 
-	return None
+	error=None
 
-@app.route('/update_Data', methods=["POST"])
-def store_data(data):
+	if request.method == "POST":
+		return "Hello"
 
-	return None
+	return "None"
+
+@app.route('/update_data', methods=["POST"])
+def update_data():
+
+	print request.values
+
+	return jsonify({"message:" : "OK"})
 
 # Run method start the flask server
 if __name__ == '__main__':
